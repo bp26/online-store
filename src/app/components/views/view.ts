a304777daede6d88e5ba1b5751040cf350725f91
bp26 <94074742+bp26@users.noreply.github.com>
2022-12-15 21:26:23 +0300

    feat: add class View

---
+++ src/app/components/views/view.ts
@@ -0,0 +1,25 @@
+import { Controller } from '../controllers/controller';
+import { ProductsView } from './productsView';
+
+export class View {
+  readonly root: HTMLElement;
+  readonly controller: Controller;
+  constructor(root: HTMLElement) {
+    this.root = root;
+    this.controller = new Controller();
+    this.mountProductsPage();
+  }
+
+  mountProductsPage(): void {
+    this.root.innerHTML = '';
+    const products = new ProductsView(this.root, this.controller.handleProductsData());
+  }
+
+  mountCartPage(): void {
+    this.root.innerHTML = '';
+  }
+
+  mountDetailsPage(): void {
+    this.root.innerHTML = '';
+  }
+}
