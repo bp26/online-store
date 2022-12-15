4f03af386b52e934f2972e724becdc5408b35050
bp26 <94074742+bp26@users.noreply.github.com>
2022-12-15 19:27:38 +0300

    feat: add class Element

---
+++ src/components/element.ts
@@ -0,0 +1,20 @@
+export class Element {
+  elem: HTMLElement;
+  constructor(parent: HTMLElement, tag = 'div', className: string, textContent: string) {
+    const elem = document.createElement(tag);
+    if (className) {
+      elem.className = className;
+    }
+    if (textContent) {
+      elem.textContent = textContent;
+    }
+    if (parent) {
+      parent.append(elem);
+    }
+    this.elem = elem;
+  }
+
+  destroy() {
+    this.elem.remove();
+  }
+}
