4b5b7219bb0eeaadf32b5505df4997020c4a7575
bp26 <94074742+bp26@users.noreply.github.com>
2022-12-16 21:51:16 +0300

    feat: add method handleProductsCallback to Controller

--- src/app/components/controllers/controller.ts
+++ src/app/components/controllers/controller.ts
@@ -1,13 +1,25 @@
 import { Model } from '../models/model';
 import { ProductsData } from '../../types/types';
+import { View } from '../views/view';
+import { ProductsAction } from '../../types/enums';
 
 export class Controller {
   readonly model: Model;
-  constructor() {
+  readonly view: View;
+  constructor(view: View) {
+    this.view = view;
     this.model = new Model();
   }
 
   handleProductsInit(): ProductsData {
     return this.model.getData();
+  }
+
+  handleProductsCallback(action: ProductsAction, id: number): void {
+    if (action === ProductsAction.add) {
+      console.log(id);
+    } else if (action === ProductsAction.details) {
+      this.view.mountDetailsPage(id);
+    }
   }
 }
