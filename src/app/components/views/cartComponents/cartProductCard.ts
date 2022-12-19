a2bf6ac719287c557df4c04182cd0988deb7c2ae
Arterixs <artemontugg@gmail.com>
2022-12-20 01:55:46 +0300

    feat: add html layuot product card

--- src/app/components/views/cartComponents/cartProductCard.ts
+++ src/app/components/views/cartComponents/cartProductCard.ts
@@ -3,15 +3,27 @@ import { IProduct } from '../../../types/interfaces';
 
 export class CartProductCard {
   constructor(node: HTMLElement, arrayProductCart: IProduct[] ) {
-    const ul = new Element(node, 'ul', 'cart-itemProduct')
+    const ul = new Element(node, 'ul', 'item - product')
     for (let i = 0; i < arrayProductCart.length; i += 1) {
-      const li = new Element(ul.elem, 'li', 'list')
-      const blockProduct = new Element(li.elem, 'div', 'list__wrapper')
-      let imageProduct = new Element(blockProduct.elem, 'img', 'wrapper__image')
+      const li = new Element(ul.elem, 'li', 'list-product')
+      const countProduct = new Element(li.elem, 'p', 'count-product', `${i + 1}`)
+      const imageBlockProduct = new Element(li.elem, 'div', 'image-block')
+      const imageProduct = new Element(imageBlockProduct.elem, 'img', 'images-product')
       imageProduct.elem.setAttribute('src', `${arrayProductCart[i].images[0]}`)
-      const nameProduct = new Element(blockProduct.elem, 'p', 'product__title', `${arrayProductCart[i].title}`)
-      const categoryProduct = new Element(blockProduct.elem, 'p', 'product__category', `${arrayProductCart[i].category}`)
-      const amountProduct = new Element(blockProduct.elem, 'p', 'product__category', `${arrayProductCart[i].price}`)
+      const descriptionBlockProduct = new Element(li.elem, 'div', 'block-base')
+      const nameProduct = new Element(descriptionBlockProduct.elem, 'p', 'product-title', `${arrayProductCart[i].title}`)
+      const descrBlock = new Element(descriptionBlockProduct.elem, 'div', 'block-description')
+      const descriptionProduct = new Element(descrBlock.elem, 'p', 'product-description',`${arrayProductCart[i].description}`)
+      const blockRatAndDisc = new Element(descrBlock.elem, 'div', 'block-data')
+      const ratingProduct = new Element(blockRatAndDisc.elem, 'p', 'product-rating', `Rating: ${arrayProductCart[i].rating}`)
+      const discountProduct = new Element(blockRatAndDisc.elem, 'p', 'product-discount', `Discount: ${arrayProductCart[i].discountPercentage}%`)
+      const blockAmountProducts = new Element(li.elem, 'div', 'block-amount')
+      const stockProduct = new Element(blockAmountProducts.elem, 'p', 'stock', `Stock: ${arrayProductCart[i].stock}`)
+      const blockCounterProduct = new Element(blockAmountProducts.elem, 'div', 'block-counter')
+      const buttonPos = new Element(blockCounterProduct.elem, 'button', 'button-count', '+')
+      const counterStocks = new Element(blockCounterProduct.elem, 'p', 'count-prod', '1')
+      const buttonNeg = new Element(blockCounterProduct.elem, 'button', 'button-count', '-')
+      const priceProduct = new Element(blockAmountProducts.elem, 'p', 'price', `$${arrayProductCart[i].price}`)
     }
   }
 }
