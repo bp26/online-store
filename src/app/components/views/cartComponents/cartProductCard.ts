20907664842fdba82b75d2c471fdcf177dafb59c
Arterixs <artemontugg@gmail.com>
2022-12-20 02:23:20 +0300

    feat: add style product card

--- src/app/components/views/cartComponents/cartProductCard.ts
+++ src/app/components/views/cartComponents/cartProductCard.ts
@@ -3,13 +3,14 @@ import { IProduct } from '../../../types/interfaces';
 
 export class CartProductCard {
   constructor(node: HTMLElement, arrayProductCart: IProduct[] ) {
-    const ul = new Element(node, 'ul', 'item - product')
+    const ul = new Element(node, 'ul', 'item-product')
     for (let i = 0; i < arrayProductCart.length; i += 1) {
       const li = new Element(ul.elem, 'li', 'list-product')
       const countProduct = new Element(li.elem, 'p', 'count-product', `${i + 1}`)
       const imageBlockProduct = new Element(li.elem, 'div', 'image-block')
       const imageProduct = new Element(imageBlockProduct.elem, 'img', 'images-product')
       imageProduct.elem.setAttribute('src', `${arrayProductCart[i].images[0]}`)
+      imageProduct.elem.setAttribute('alt', `${arrayProductCart[i].title}`)
       const descriptionBlockProduct = new Element(li.elem, 'div', 'block-base')
       const nameProduct = new Element(descriptionBlockProduct.elem, 'p', 'product-title', `${arrayProductCart[i].title}`)
       const descrBlock = new Element(descriptionBlockProduct.elem, 'div', 'block-description')
@@ -20,9 +21,9 @@ export class CartProductCard {
       const blockAmountProducts = new Element(li.elem, 'div', 'block-amount')
       const stockProduct = new Element(blockAmountProducts.elem, 'p', 'stock', `Stock: ${arrayProductCart[i].stock}`)
       const blockCounterProduct = new Element(blockAmountProducts.elem, 'div', 'block-counter')
-      const buttonPos = new Element(blockCounterProduct.elem, 'button', 'button-count', '+')
-      const counterStocks = new Element(blockCounterProduct.elem, 'p', 'count-prod', '1')
       const buttonNeg = new Element(blockCounterProduct.elem, 'button', 'button-count', '-')
+      const counterStocks = new Element(blockCounterProduct.elem, 'p', 'count-prod', '1')
+      const buttonPos = new Element(blockCounterProduct.elem, 'button', 'button-count', '+')
       const priceProduct = new Element(blockAmountProducts.elem, 'p', 'price', `$${arrayProductCart[i].price}`)
     }
   }
