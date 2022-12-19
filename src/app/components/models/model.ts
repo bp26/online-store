c144a38f902e9962e4e9d54ada3a73d3bf6edbb5
bp26 <94074742+bp26@users.noreply.github.com>
2022-12-19 22:51:46 +0300

    feat: initialize Filter model

--- src/app/components/models/model.ts
+++ src/app/components/models/model.ts
@@ -1,17 +1,25 @@
 import { ProductsData } from '../../types/types';
 import products from '../../../assets/data/products';
 import { Cart } from './cart';
+import { Filter } from './filter';
+import { IFilterOptions } from '../../types/interfaces';
 
 export class Model {
   readonly data: ProductsData;
   readonly cart: Cart;
+  readonly filter: Filter;
 
   constructor() {
     this.data = products;
     this.cart = new Cart();
+    this.filter = new Filter();
   }
 
-  getData(): ProductsData {
+  public getData(): ProductsData {
     return this.data;
+  }
+
+  public getFilterOptions(): IFilterOptions {
+    return this.filter.setFilterOptions(this.getData(), this.getData());
   }
 }
