8c05efc8e8a7d0ee138cfb8d5012fa4bb647ded3
bp26 <aleshak260006@gmail.com>
2022-12-19 16:04:56 +0300

    feat: add types ListFilters, CountFilters, ListOption, CountOption

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -8,3 +8,17 @@ export type ProductsCallback = (action: ProductsAction, id: number) => void;
 export type CartList = {
   [key: string]: number;
 };
+
+export type ListFilters = 'category' | 'brand';
+export type CountFilters = 'price' | 'stock';
+
+export type ListOption = {
+  [key: string]: {
+    current: number;
+    full: number;
+  };
+};
+export type CountOption = {
+  min: number;
+  max: number;
+};
