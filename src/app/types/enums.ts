095c8507e5d9549beae831d79390a6d0a79b2797
bp26 <aleshak260006@gmail.com>
2022-12-20 15:28:10 +0300

    feat: add enum FiltersAction

--- src/app/types/enums.ts
+++ src/app/types/enums.ts
@@ -3,6 +3,15 @@ export enum ProductsAction {
   details,
 }
 
+export enum FiltersAction {
+  category,
+  brand,
+  price,
+  stock,
+  reset,
+  copy,
+}
+
 export enum CardButtonTitles {
   add = 'ADD TO CART',
   remove = 'REMOVE FROM CART',
