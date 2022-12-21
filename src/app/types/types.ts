930a711b46fbcbf47ebe2d851b2b78d8ff6753af
bp26 <aleshak260006@gmail.com>
2022-12-21 08:43:50 +0300

    feat: change 2nd argument of FiltersCallback type

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -6,7 +6,7 @@ import { FiltersType } from './enums';
 export type ProductsData = IProduct[];
 
 export type ProductsCallback = (action: ProductsAction, id: number) => void;
-export type FiltersCallback = (action: FiltersAction, filter?: string | CountOptions) => void;
+export type FiltersCallback = (action: FiltersAction, filtersData?: FiltersData) => void;
 
 export type CartList = {
   [key: string]: number;
