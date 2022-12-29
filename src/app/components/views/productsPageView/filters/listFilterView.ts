a1ca168504a1ec77d37049a00610607b1519ee40
bp26 <94074742+bp26@users.noreply.github.com>
2022-12-29 16:35:36 +0300

    feat: unify listFilterView css classes under filters-list

--- src/app/components/views/productsPageView/filters/listFilterView.ts
+++ src/app/components/views/productsPageView/filters/listFilterView.ts
@@ -8,13 +8,13 @@ import { FiltersType } from '../../../../types/enums';
 export class ListFilterView extends Element {
   private list: Element;
   constructor(parent: HTMLElement, filterName: ListFilterNames, filterOptions: ListOptions, callback: FiltersCallback) {
-    super(parent, 'div', `filters-list filters-${filterName}`);
+    super(parent, 'div', `filters-list`);
 
-    const upperWrapper = new Element(this.elem, 'div', `filter-${filterName}__upper-wrapper`);
-    const name = new Element(upperWrapper.elem, 'span', `filters-${filterName}__name`, `${filterName}`);
+    const upperWrapper = new Element(this.elem, 'div', `filter-list__upper-wrapper`);
+    const name = new Element(upperWrapper.elem, 'span', `filters-list__name`, `${filterName}`);
 
-    const bottomWrapper = new Element(this.elem, 'div', `filters-${filterName}__bottom-wrapper`);
-    this.list = new Element(bottomWrapper.elem, 'ul', `fiters-${filterName}__list`);
+    const bottomWrapper = new Element(this.elem, 'div', `filters-list__bottom-wrapper`);
+    this.list = new Element(bottomWrapper.elem, 'ul', `fiters-list__list`);
 
     this.renderOptions(filterName, filterOptions, callback);
   }
@@ -22,9 +22,9 @@ export class ListFilterView extends Element {
   public renderOptions(filterName: ListFilterNames, filterOptions: ListOptions, callback: FiltersCallback) {
     this.list.elem.innerHTML = '';
     for (const filterOption in filterOptions) {
-      const option = new Element(this.list.elem, 'li', `filters-${filterName}__option`);
+      const option = new Element(this.list.elem, 'li', `filters-list__option`);
 
-      const input = new Element(option.elem, 'input', `filters-${filterName}__checkbox`);
+      const input = new Element(option.elem, 'input', `filters-list__checkbox`);
       input.elem.setAttribute('type', 'checkbox');
       (input.elem as HTMLInputElement).checked = filterOptions[filterOption].isChecked;
       input.elem.setAttribute('id', `${filterOption}`);
@@ -32,10 +32,10 @@ export class ListFilterView extends Element {
         callback(FiltersAction.filter, { type: FiltersType[filterName], filter: filterOption });
       };
 
-      const label = new Element(option.elem, 'label', `filters-${filterName}__label`, `${filterOption}`);
+      const label = new Element(option.elem, 'label', `filters-list__label`, `${filterOption}`);
       label.elem.setAttribute('id', `${filterOption}`);
 
-      const span = new Element(option.elem, 'span', `filters-${filterName}__count`, `${filterOptions[filterOption].current}/${filterOptions[filterOption].full}`);
+      const span = new Element(option.elem, 'span', `filters-list__count`, `${filterOptions[filterOption].current}/${filterOptions[filterOption].full}`);
     }
   }
 }
