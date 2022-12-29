3c30806f978ee76c3d15c6a69f6d7a8d40b7704b
bp26 <94074742+bp26@users.noreply.github.com>
2022-12-29 14:41:44 +0300

    feat:  initialize priceFilter and stockFilter in FiltersView

--- src/app/components/views/filters/filtersView.ts
+++ src/app/components/views/filters/filtersView.ts
@@ -1,13 +1,20 @@
 import { Element } from '../../element';
 import { IFilterOptions } from '../../../types/interfaces';
 import { ListFiltersView } from './listFilterView';
+import { CountFiltersView } from './countFilterView';
 import { FiltersCallback } from '../../../types/types';
 
 export class FiltersView extends Element {
+  private categoryFilter: ListFiltersView;
+  private brandFilter: ListFiltersView;
+  private priceFilter: CountFiltersView;
+  private stockFilter: CountFiltersView;
   constructor(parent: HTMLElement, options: IFilterOptions, callback: FiltersCallback) {
     super(parent, 'div', 'filters');
     const { category, brand, price, stock } = options;
-    const categoryFilter = new ListFiltersView(this.elem, 'category', category, callback);
-    const brandFilter = new ListFiltersView(this.elem, 'brand', brand, callback);
+    this.categoryFilter = new ListFiltersView(this.elem, 'category', category, callback);
+    this.brandFilter = new ListFiltersView(this.elem, 'brand', brand, callback);
+    this.priceFilter = new CountFiltersView(this.elem, 'price', price, callback);
+    this.stockFilter = new CountFiltersView(this.elem, 'stock', stock, callback);
   }
 }
