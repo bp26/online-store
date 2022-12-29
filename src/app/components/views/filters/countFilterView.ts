61b34f250a43af7993897a1f646db77c0a5c6dac
bp26 <94074742+bp26@users.noreply.github.com>
2022-12-29 14:46:27 +0300

    feat: add methods toggleValues and setValues in CountFiltersView

--- src/app/components/views/filters/countFilterView.ts
+++ src/app/components/views/filters/countFilterView.ts
@@ -7,16 +7,20 @@ import { FiltersCallback } from '../../../types/types';
 import { FiltersType } from '../../../types/enums';
 
 export class CountFiltersView extends Element {
-  sliderOne: FilterSlider;
-  sliderTwo: FilterSlider;
-  minValue: Element;
-  maxValue: Element;
+  private sliderOne: FilterSlider;
+  private sliderTwo: FilterSlider;
+  private emptyValue: Element;
+  private minValue: Element;
+  private maxValue: Element;
+
   constructor(parent: HTMLElement, filterName: CountFilterNames, options: CountOptions, callback: FiltersCallback) {
     super(parent, 'div', `filters-${filterName}`);
 
     const valuesContainer = new Element(this.elem, 'div', `filters-${filterName}__values-container`);
+    this.emptyValue = new Element(valuesContainer.elem, 'p', `filters-${filterName}__empty-value`, 'NOT FOUND');
     this.minValue = new Element(valuesContainer.elem, 'div', `filters-${filterName}__value`, `${options.min}`);
     this.maxValue = new Element(valuesContainer.elem, 'div', `filters-${filterName}__value`, `${options.max}`);
+    this.toggleValues(options);
 
     const slidersContainer = new Element(this.elem, 'div', `filters-${filterName}__sliders-container`);
     this.sliderOne = new FilterSlider(slidersContainer.elem, `filters-${filterName}__slider`, options.start, options.end, options.min);
@@ -36,12 +40,23 @@ export class CountFiltersView extends Element {
     return [valueOne, valueTwo];
   }
 
-  public update(options: CountOptions): void {
+  private toggleValues(options: CountOptions): void {
+    this.emptyValue.elem.style.display = options.isEmpty ? 'block' : 'none';
+    this.minValue.elem.style.display = options.isEmpty ? 'none' : 'block';
+    this.maxValue.elem.style.display = options.isEmpty ? 'none' : 'block';
+  }
+
+  private setValues(options: CountOptions) {
     this.minValue.elem.textContent = `${options.min}`;
     this.maxValue.elem.textContent = `${options.max}`;
     if (!options.isActive) {
       this.sliderOne.setValue(options.min);
       this.sliderTwo.setValue(options.max);
     }
+  }
+
+  public update(options: CountOptions): void {
+    this.toggleValues(options);
+    this.setValues(options);
   }
 }
