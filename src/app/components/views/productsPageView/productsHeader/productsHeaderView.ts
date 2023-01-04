d280df98239b45dff4704a796ad090e48ef5c572
bp26 <94074742+bp26@users.noreply.github.com>
2023-01-04 23:49:29 +0300

    feat: initialize options and callbacks in ProductsHeaderView

--- src/app/components/views/productsPageView/productsHeader/productsHeaderView.ts
+++ src/app/components/views/productsPageView/productsHeader/productsHeaderView.ts
@@ -1,19 +1,25 @@
 import { Element } from '../../../element';
 import { HTMLTag } from '../../../../types/enums';
 import { SortView } from './sortView';
+import { SearchView } from './searchView';
+import { IProductsHeaderOptions } from '../../../../types/interfaces';
+import { IProductsHeaderCallbacks } from '../../../../types/interfaces';
 
 export class ProductsHeaderView extends Element {
   productsCount: Element;
-  constructor(parent: HTMLElement) {
+  constructor(parent: HTMLElement, options: IProductsHeaderOptions, callbacks: IProductsHeaderCallbacks) {
     super(parent, HTMLTag.DIV, `products-header`);
 
-    //const sortList = new SortView(this.elem);
-    this.productsCount = new Element(this.elem, HTMLTag.SPAN, 'products-header__count');
-    const searchInput = new Element(this.elem, HTMLTag.INPUT, 'products-header__search');
+    const { sortType, productsCount } = options;
+    const { sortCallback, searchCallback, toggleViewCallback } = callbacks;
+
+    const sortList = new SortView(this.elem, sortType, sortCallback);
+    this.productsCount = new Element(this.elem, HTMLTag.SPAN, 'products-header__count', String(productsCount));
+    const searchInput = new SearchView(this.elem, searchCallback);
     const viewToggle = new Element(this.elem, HTMLTag.DIV, 'products-header__view');
   }
 
   public updateCount(count: number): void {
-    this.productsCount.elem.textContent = count + '';
+    this.productsCount.elem.textContent = String(count);
   }
 }
