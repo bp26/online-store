8a2c7f30e50478fb1e43e59184050db1d488bd9b
bp26 <94074742+bp26@users.noreply.github.com>
2023-01-06 22:26:07 +0300

    feat: add placeholder to input in SearchView

--- src/app/components/views/productsPageView/productsHeader/searchView.ts
+++ src/app/components/views/productsPageView/productsHeader/searchView.ts
@@ -1,6 +1,7 @@
 import { Element } from '../../../element';
 import { HTMLTag } from '../../../../types/enums';
 import { SearchCallback } from '../../../../types/types';
+import { SEARCH_PLACEHOLDER } from '../../../../utils/constants';
 
 export class SearchView extends Element {
   constructor(parent: HTMLElement, callback: SearchCallback) {
@@ -11,6 +12,8 @@ export class SearchView extends Element {
     if (!(input instanceof HTMLInputElement)) {
       throw new Error();
     }
+
+    input.placeholder = SEARCH_PLACEHOLDER;
 
     input.oninput = () => {
       input.value = input.value.toLowerCase();
