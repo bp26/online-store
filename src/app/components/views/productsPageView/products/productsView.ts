dcf6adaea11ba2bc23ed8854e552c39da5d5d4e7
bp26 <94074742+bp26@users.noreply.github.com>
2023-01-06 22:23:24 +0300

    feat: add method toggleDisplay to ProductsView

--- src/app/components/views/productsPageView/products/productsView.ts
+++ src/app/components/views/productsPageView/products/productsView.ts
@@ -3,21 +3,31 @@ import { ProductsCallback } from '../../../../types/types';
 import { HTMLTag } from '../../../../types/enums';
 import { IProductsOptions } from '../../../../types/interfaces';
 import { Product } from './product';
+import { ProductDisplay } from '../../../../types/enums';
 
 export class ProductsView extends Element {
   constructor(parent: HTMLElement, productsOptions: IProductsOptions, callback: ProductsCallback) {
     super(parent, HTMLTag.DIV, 'products');
+    this.toggleDisplay(productsOptions.productDisplay);
     this.renderProducts(productsOptions, callback);
   }
 
-  public renderProducts({ data, cartArray, productDisplay }: IProductsOptions, callback: ProductsCallback): void {
+  public renderProducts({ data, cartArray }: IProductsOptions, callback: ProductsCallback): void {
     this.elem.innerHTML = '';
     if (data.length !== 0) {
       data.forEach((product) => {
-        const productView = new Product(this.elem, product, cartArray.includes(product.id), productDisplay, callback);
+        const productView = new Product(this.elem, product, cartArray.includes(product.id), callback);
       });
     } else {
       const emptyMessage = new Element(this.elem, HTMLTag.P, 'products__empty-message', 'NOT FOUND');
+    }
+  }
+
+  public toggleDisplay(productDisplay: ProductDisplay) {
+    if (productDisplay === ProductDisplay.SIMPLE) {
+      this.elem.classList.add('products_simple');
+    } else {
+      this.elem.classList.remove('products_simple');
     }
   }
 }
