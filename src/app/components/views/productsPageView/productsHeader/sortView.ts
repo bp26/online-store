a2b23babfce60b4c5debcc3eade497a1be62e587
bp26 <94074742+bp26@users.noreply.github.com>
2023-01-06 22:37:52 +0300

    feat: update SortView

--- src/app/components/views/productsPageView/productsHeader/sortView.ts
+++ src/app/components/views/productsPageView/productsHeader/sortView.ts
@@ -8,15 +8,29 @@ export class SortView extends Element {
     super(parent, HTMLTag.DIV, 'products-header__sort');
 
     const dropbtn = new Element(this.elem, HTMLTag.DIV, 'products-header__dropbtn', type);
-
-    const droplist = new Element(this.elem, HTMLTag.DIV, 'products-header__droplist products-header__droplist_hidden');
+    const droplist = new Element(this.elem, HTMLTag.UL, 'products-header__droplist');
 
     Object.values(SortType).forEach((option) => {
-      const dropoption = new Element(droplist.elem, HTMLTag.DIV, 'products-header__dropoption', option);
+      const dropoption = new Element(droplist.elem, HTMLTag.LI, 'products-header__dropoption', option);
       dropoption.elem.onclick = () => {
         dropbtn.elem.textContent = option;
+        droplist.elem.classList.remove('products-header__droplist_visible');
         callback(option);
       };
     });
+
+    dropbtn.elem.onclick = () => {
+      droplist.elem.classList.toggle('products-header__droplist_visible');
+    };
+
+    document.body.onclick = (e) => {
+      if (!(e.target instanceof HTMLElement)) {
+        throw new Error();
+      }
+
+      if (!this.elem.contains(e.target)) {
+        droplist.elem.classList.remove('products-header__droplist_visible');
+      }
+    };
   }
 }
