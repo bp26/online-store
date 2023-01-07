f91926fd0a5e963a3c801fd0874e06b28a3a8104
bp26 <94074742+bp26@users.noreply.github.com>
2023-01-07 21:00:24 +0300

    feat: add callback to arguments of constructor in BreadcrumbsView

--- src/app/components/views/detailsPageView/breadcrumbsView.ts
+++ src/app/components/views/detailsPageView/breadcrumbsView.ts
@@ -1,14 +1,16 @@
 import { Element } from '../../element';
-import { HTMLTag } from '../../../types/enums';
+import { DetailsAction, HTMLTag } from '../../../types/enums';
 import { IProduct } from '../../../types/interfaces';
 import { BREADCRUMBS_LIST } from '../../../utils/constants';
 import { BreadcrumbsElements } from '../../../types/enums';
+import { DetailsCallback } from '../../../types/types';
 
 export class BreadcrumbsView extends Element {
-  constructor(parent: HTMLElement, productData: IProduct) {
+  constructor(parent: HTMLElement, productData: IProduct, callback: DetailsCallback) {
     super(parent, HTMLTag.DIV, 'details-page__breadcrumbs breadcrumbs');
 
     const store = new Element(this.elem, HTMLTag.SPAN, 'breadcrumbs__item', BreadcrumbsElements.STORE);
+    store.elem.onclick = () => callback(DetailsAction.BACK);
 
     BREADCRUMBS_LIST.forEach((item) => {
       const separator = new Element(this.elem, HTMLTag.SPAN, 'breadcrumbs__separator', BreadcrumbsElements.SEPARATOR);
