7727c6ba3b14174fc4ca51c30b922792a491f9e5
bp26 <94074742+bp26@users.noreply.github.com>
2023-01-08 17:00:37 +0300

    feat: update DetailsCartView

--- src/app/components/views/detailsPageView/detailsCartView.ts
+++ src/app/components/views/detailsPageView/detailsCartView.ts
@@ -2,17 +2,30 @@ import { Element } from '../../element';
 import { HTMLTag } from '../../../types/enums';
 import { DetailsAction } from '../../../types/enums';
 import { DetailsCallback } from '../../../types/types';
+import { CardButtonTitles } from '../../../types/enums';
 
 export class DetailsCartView extends Element {
-  constructor(parent: HTMLElement, id: number, productPrice: number, inCart: boolean, callback: DetailsCallback) {
+  constructor(parent: HTMLElement, id: number, productPrice: number, private inCart: boolean, callback: DetailsCallback) {
     super(parent, HTMLTag.DIV, 'details-page__details-cart details-cart');
 
     const price = new Element(this.elem, HTMLTag.SPAN, 'details-cart__price', `€${productPrice}`);
 
     const addButton = new Element(this.elem, HTMLTag.BUTTON, 'details-cart__button', 'ADD');
-    addButton.elem.onclick = () => callback(DetailsAction.ADD, id, productPrice);
+    addButton.elem.onclick = () => {
+      callback(DetailsAction.ADD, id, productPrice);
+      this.inCart = !this.inCart;
+      this.setCartDisplay(addButton.elem);
+    };
 
-    const buyButton = new Element(this.elem, HTMLTag.BUTTON, 'details-cart__button', 'BUY NOW');
+    const buyButton = new Element(this.elem, HTMLTag.BUTTON, 'details-cart__button', CardButtonTitles.BUY);
     buyButton.elem.onclick = () => callback(DetailsAction.BUY, id, productPrice);
+  }
+
+  private setCartDisplay(addButton: HTMLElement): void {
+    if (this.inCart) {
+      addButton.textContent = CardButtonTitles.REMOVE;
+    } else {
+      addButton.textContent = CardButtonTitles.ADD;
+    }
   }
 }
